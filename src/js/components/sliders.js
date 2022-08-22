c49ff884d4a77dfea9dff010cca49696179a1d13
SlavaK <SlavaK@excample.com>
2022-08-22 14:57:38 +1000

    services-inner_related-projects

--- src/js/components/sliders.js
+++ src/js/components/sliders.js
@@ -4,6 +4,7 @@ const bodyStyles = window.getComputedStyle(document.body);
 const gap = parseInt(bodyStyles.getPropertyValue('--grid-gap'));
 
 const portSlider = document.querySelector('.portfolio-section__items');
+const relatedSlider = document.querySelector('.related-projects__items');
 
 if (portSlider) {
     const portfolioSlider = new Swiper(portSlider, {
@@ -52,6 +53,62 @@ if (portSlider) {
         const nextNextActiveSlide = nextActiveSlide.nextElementSibling;
 
         document.querySelectorAll('.portfolio-section__items .swiper-slide').forEach(el => {
+            el.classList.remove('slider-visible');
+        });
+
+        activeSlide.classList.add('slider-visible');
+        nextActiveSlide.classList.add('slider-visible');
+        nextNextActiveSlide.classList.add('slider-visible');
+    });
+}
+
+if (relatedSlider) {
+    const relatedProjSlider = new Swiper(relatedSlider, {
+        modules: [Navigation, Pagination],
+        slidesPerView: 3,
+        spaceBetween: gap,
+        on: {
+            init: function () {
+                const activeSlide = relatedSlider.querySelector('.swiper-slide-active');
+
+                const nextActiveSlide = activeSlide.nextElementSibling;
+
+                const nextNextActiveSlide = nextActiveSlide.nextElementSibling;
+
+                activeSlide.classList.add('slider-visible');
+                nextActiveSlide.classList.add('slider-visible');
+                nextNextActiveSlide.classList.add('slider-visible');
+            },
+        },
+        navigation: {
+            nextEl: '.related-projects__next',
+            prevEl: '.related-projects__prev',
+        },
+    });
+
+    document.querySelector('.related-projects__prev').addEventListener('click', () => {
+        const activeSlide = relatedSlider.querySelector('.swiper-slide-next');
+
+        document.querySelectorAll('.related-projects__items .swiper-slide').forEach(el => {
+            el.classList.remove('slider-visible');
+        });
+
+        if (activeSlide.previousElementSibling) {
+            const nextActiveSlide = activeSlide.previousElementSibling;
+            activeSlide.classList.add('slider-visible');
+            nextActiveSlide.classList.add('slider-visible');
+            activeSlide.nextElementSibling.classList.add('slider-visible');
+        }
+    });
+
+    document.querySelector('.related-projects__next').addEventListener('click', () => {
+        const activeSlide = relatedSlider.querySelector('.swiper-slide-active');
+
+        const nextActiveSlide = activeSlide.nextElementSibling;
+
+        const nextNextActiveSlide = nextActiveSlide.nextElementSibling;
+
+        document.querySelectorAll('.related-projects__items .swiper-slide').forEach(el => {
             el.classList.remove('slider-visible');
         });
 
